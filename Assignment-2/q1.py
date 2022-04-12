d4a1faaa34dc4963f20cc658756e8d3ad3e94d0a
vaishakhi123 <31160710+vaishakhi123@users.noreply.github.com>
2022-04-12 22:24:20 +0530

    Add files via upload

--- Assignment-2/q1.py
+++ Assignment-2/q1.py
@@ -1,19 +1,51 @@
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 sys.path.insert(0, '../')
 from vaishakhi.reg import poly_reg
 from vaishakhi.elimination_solvers import *
+from vaishakhi.iterative_solvers import *
+
+def chebyshev(x):
+    phi = [1, 2*x-1, 8*x**2-8*x+1, 32*x**3-48*x**2+18*x-1] 
+    return phi
+
+def func(x,a,n,solve):
+
+    if solve == "poly":
+        return sum([a[i]*np.power(x,i) for i in range(n+1)])
+    else: 
+        return sum([a[i]*chebyshev(x)[i] for i in range(n+1)])        
+            
+
+n = 3
 x,y = np.genfromtxt("ass2.txt", delimiter="    ", unpack=True, dtype = float)
-A,b = poly_reg(x,y,np.ones(len(y)),3)
+A,b = poly_reg(x,y,np.ones(len(y)),n)
+N = len(x)
 
-#finding the parameters Ax=b using elimination method 
- 
+#finding the fitting parameters Ax=b using elimination method and y from polynomial regression
 a = lu_dolittle(A,b)
-print("The fitting parameters for the polynomial fit are",a)
-#print(np.polyfit(x, y, 3))
+y_poly = func(x,a,n,"poly")
 
-B = np.array([[1.0,-1.0,1.0,1.0],[0,2.0,-8.0,18.0],[0,0,8.0,48.0],[0,0,0,32.0]])
-c = np.array([  0.57465867 ,  4.72586144 ,-11.12821778 ,  7.66867762])
-print(back_substitution(B,c))
-print(np.linalg.cond(A),np.linalg.cond(B))
+print("The fitting parameters for the polynomial fit are",a)
+
+B = np.zeros((n+1,n+1))
+for j in range(n+1):
+    for k in range(n+1):
+        B[j,k] = np.sum([(chebyshev(x[i])[k]*chebyshev(x[i])[j]) for i in range(N)])
+    b[j] = np.sum([(chebyshev(x[i])[j]*y[i]) for i in range(N)])
+
+#obtaining the fitted y using modified basis 
+print("The fitting parameters for modified chebyshev are", lu_dolittle(B,b))
+y_cheb = func(x,lu_dolittle(B,b),n,"cheb")
+
+p1 = plt.plot(x,y,'o')
+p2 = plt.plot(x,y_poly, 'b-')
+p3 = plt.plot(x,y_cheb,'g-')
+plt.legend(["Original","Polynomial fit", "Modified Chebyshev"],loc="lower right")
+plt.savefig("q1.png")
+print("The condition number for matrices for polynomial regression and modified chebyshev are {} and {} respectively".format(np.round(np.linalg.cond(A),4),np.round(np.linalg.cond(B),4)))
+print("The lower conditional number of modified basis method tells us about the numerical stability and accuracy")
+
+
